454f5e91e9793e3dafc03db47155dd2068f71453
AJAY <inavolu.a@northeastern.edu>
2023-10-14 16:06:09 -0400

    Improved error handling and response parsing in performSearch. Separated success, error, and exception handling into dedicated methods.

--- app/src/main/java/edu/northeastern/numad23fa23_group5/AtYourServiceActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa23_group5/AtYourServiceActivity.java
@@ -24,6 +24,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -60,12 +61,12 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_you_service);
 
-        editTextKeyword=findViewById(R.id.textInputKeyword);
-        editTextMinPrice=findViewById(R.id.minPriceEditText);
-        editTextMaxPrice=findViewById(R.id.maxPriceEditText);
-        checkBoxIfCache=findViewById(R.id.checkBoxCaching);
-        toggleButtonCountry=findViewById(R.id.toggleCountry);
-        progressBar=findViewById(R.id.progressLoader);
+        editTextKeyword = findViewById(R.id.textInputKeyword);
+        editTextMinPrice = findViewById(R.id.minPriceEditText);
+        editTextMaxPrice = findViewById(R.id.maxPriceEditText);
+        checkBoxIfCache = findViewById(R.id.checkBoxCaching);
+        toggleButtonCountry = findViewById(R.id.toggleCountry);
+        progressBar = findViewById(R.id.progressLoader);
         Button btnSearch = findViewById(R.id.btnSearch);
         spinner = (Spinner) findViewById(R.id.spinnerSorting);
 
@@ -105,8 +106,8 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
         toggleButtonCountry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String current=toggleButtonCountry.isChecked()?"CAD":"USD";
-                String minUnit=current+" to";
+                String current = toggleButtonCountry.isChecked() ? "CAD" : "USD";
+                String minUnit = current + " to";
                 ((TextView) findViewById(R.id.minPriceTextView)).setText(minUnit);
                 ((TextView) findViewById(R.id.maxPriceTextView)).setText(current);
             }
@@ -120,7 +121,7 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
         outState.putString(KEY_MAX_PRICE, editTextMaxPrice.getText().toString());
         outState.putInt(KEY_SORT_TYPE_POSITION, spinner.getSelectedItemPosition());
         outState.putBoolean(KEY_COUNTRY, toggleButtonCountry.isChecked());
-        Log.d("onSaveInstanceState",toggleButtonCountry.isChecked()+"");
+        Log.d("onSaveInstanceState", toggleButtonCountry.isChecked() + "");
         outState.putBoolean(KEY_IF_DISABLE_CACHE, checkBoxIfCache.isChecked());
         super.onSaveInstanceState(outState);
     }
@@ -137,10 +138,10 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
             editTextMaxPrice.setText(savedInstanceState.getString(KEY_MAX_PRICE));
             spinner.setSelection(savedInstanceState.getInt(KEY_SORT_TYPE_POSITION));
             toggleButtonCountry.setChecked(savedInstanceState.getBoolean(KEY_COUNTRY));
-            Log.d("initialData",savedInstanceState.getBoolean(KEY_COUNTRY)+"");
-            Log.d("initialData",toggleButtonCountry.isChecked()+"");
-            String current=savedInstanceState.getBoolean(KEY_COUNTRY)?"CAD":"USD";
-            String minUnit=current+" to";
+            Log.d("initialData", savedInstanceState.getBoolean(KEY_COUNTRY) + "");
+            Log.d("initialData", toggleButtonCountry.isChecked() + "");
+            String current = savedInstanceState.getBoolean(KEY_COUNTRY) ? "CAD" : "USD";
+            String minUnit = current + " to";
             ((TextView) findViewById(R.id.minPriceTextView)).setText(minUnit);
             ((TextView) findViewById(R.id.maxPriceTextView)).setText(current);
             checkBoxIfCache.setChecked(savedInstanceState.getBoolean(KEY_IF_DISABLE_CACHE));
@@ -153,7 +154,7 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
                                int pos, long id) {
         // An item is selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos).
-        sortType=parent.getItemAtPosition(pos).toString();
+        sortType = parent.getItemAtPosition(pos).toString();
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -162,15 +163,17 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
         // The selection can disappear for instance when touch is activated or when the adapter becomes empty.
     }
 
-    private void performSearch()
-    {
-        country=toggleButtonCountry.isChecked()?"Canada":"USA";
+    private void performSearch() {
+        country = toggleButtonCountry.isChecked() ? "Canada" : "USA";
         //get user input
         //keyword
-        keyword=editTextKeyword.getText().toString();
+        keyword = editTextKeyword.getText().toString();
+        //min price
+        minPrice = editTextMinPrice.getText().toString();
+        //max price
+        maxPrice = editTextMaxPrice.getText().toString();
         //check if search keyword is empty
-        if(keyword.isEmpty())
-        {
+        if (keyword.isEmpty()) {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
@@ -180,36 +183,32 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
                 }
             });
         }
-        //min price
-        minPrice=editTextMinPrice.getText().toString();
-        //max price
-        maxPrice=editTextMaxPrice.getText().toString();
-        switch (sortType)
-        {
+
+        switch (sortType) {
             case "Top Sellers":
-                sortType=(country.equals("USA"))?"top_sellers":"priceSaving";
+                sortType = (country.equals("USA")) ? "top_sellers" : "priceSaving";
                 break;
             case "Price Low to High":
-                sortType=(country.equals("USA"))?"price_low_to_high":"price-asc";
+                sortType = (country.equals("USA")) ? "price_low_to_high" : "price-asc";
                 break;
             case "Price High to Low":
-                sortType=(country.equals("USA"))?"price_high_to_low":"price-desc";
+                sortType = (country.equals("USA")) ? "price_high_to_low" : "price-desc";
                 break;
             case "Top Rated":
-                sortType=(country.equals("USA"))?"top_rated":"reviewAvgRating";
+                sortType = (country.equals("USA")) ? "top_rated" : "reviewAvgRating";
                 break;
             case "Relevance":
-                sortType=(country.equals("USA"))?"best_match":"relevance";
+                sortType = (country.equals("USA")) ? "best_match" : "relevance";
                 break;
         }
-        String sortTypeSearchKey=(country.equals("USA"))?"hd_sort":"sort";
-        ifDisableCache=checkBoxIfCache.isChecked();
-        Log.d("performSearchCountry",country);
-        Log.d("performSearchKeyword",keyword);
-        Log.d("performSearchMinPrice",minPrice);
-        Log.d("performSearchMaxPrice",maxPrice);
-        Log.d("performSearchSortType",sortType);
-        Log.d("performSearchSortKey",sortTypeSearchKey);
+        String sortTypeSearchKey = (country.equals("USA")) ? "hd_sort" : "sort";
+        ifDisableCache = checkBoxIfCache.isChecked();
+        Log.d("performSearchCountry", country);
+        Log.d("performSearchKeyword", keyword);
+        Log.d("performSearchMinPrice", minPrice);
+        Log.d("performSearchMaxPrice", maxPrice);
+        Log.d("performSearchSortType", sortType);
+        Log.d("performSearchSortKey", sortTypeSearchKey);
         Log.d("performSearchNoCache", String.valueOf(ifDisableCache));
 
         //USA
@@ -237,97 +236,116 @@ public class AtYourServiceActivity extends AppCompatActivity implements AdapterV
         //              Example: price:[0 TO 500](Maximum $500)
         //no_cache      $ifDisableCache (false/true)
 
-        try{
+        try {
 
-            String api_key="46b4efc39069e71e94b9df0cc639c4fb01951988f2a312425fdaf43cdf1b807d";
-            String baseURL="https://serpapi.com/search.json?engine=home_depot";
+            String api_key = "46b4efc39069e71e94b9df0cc639c4fb01951988f2a312425fdaf43cdf1b807d";
+            String baseURL = "https://serpapi.com/search.json?engine=home_depot";
             String getParams;
-            if(country.equals("USA"))
-            {
-                getParams=String.format("&q=%s&api_key=%s&country=us&hd_sort=%s&no_cache=%b",keyword,api_key,sortType,ifDisableCache);
-                if(!minPrice.isEmpty())
-                {
-                    getParams=getParams+"&lowerbound="+minPrice;
+            if (country.equals("USA")) {
+                getParams = String.format("&q=%s&api_key=%s&country=us&hd_sort=%s&no_cache=%b", keyword, api_key, sortType, ifDisableCache);
+                if (!minPrice.isEmpty()) {
+                    getParams = getParams + "&lowerbound=" + minPrice;
                 }
-                if(!maxPrice.isEmpty())
-                {
-                    getParams=getParams+"&upperbound="+maxPrice;
+                if (!maxPrice.isEmpty()) {
+                    getParams = getParams + "&upperbound=" + maxPrice;
                 }
-            }
-            else{
-                getParams=String.format("&q=%s&api_key=%s&country=ca&sort=%s&no_cache=%b",keyword,api_key,sortType,ifDisableCache);
-                if(!minPrice.isEmpty()||!maxPrice.isEmpty())
-                {
-                    minPrice=(minPrice.isEmpty())?"*":minPrice;
-                    maxPrice=(maxPrice.isEmpty())?"*":maxPrice;
-                    getParams=getParams+String.format("&minmax=[%s TO %s]",minPrice,maxPrice);
+            } else {
+                getParams = String.format("&q=%s&api_key=%s&country=ca&sort=%s&no_cache=%b", keyword, api_key, sortType, ifDisableCache);
+                if (!minPrice.isEmpty() || !maxPrice.isEmpty()) {
+                    minPrice = (minPrice.isEmpty()) ? "*" : minPrice;
+                    maxPrice = (maxPrice.isEmpty()) ? "*" : maxPrice;
+                    getParams = getParams + String.format("&minmax=[%s TO %s]", minPrice, maxPrice);
                 }
             }
-            URL url=new URL(baseURL+getParams);
+            URL url = new URL(baseURL + getParams);
             /// a url that gives feedback in given amount delay time, to test the loading progress bar.
 //            url = new URL("https://jsonplaceholder.typicode.com/posts?_delay=5000");
-            Log.d("performSearchHomeDepotURL",url.toString());
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                }
-                bufferedReader.close();
-                final String response = stringBuilder.toString();
-                Log.d("performSearchResponse",response);
-//                JSONObject jsonObjectResponse=new JSONObject(response);
-//                Log.d("performSearchProductsPosition ", jsonObjectResponse.getJSONArray("products").toString());
-//                JSONArray resultProducts=jsonObjectResponse.getJSONArray("products");
-//                for (int i = 0; i < resultProducts.length(); i++) {
-//                    Log.d("performSearchProductTitle",resultProducts.getJSONObject(i).getString("title"));
-//                }
-                // Process the response on the main UI thread
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        View rootView = findViewById(android.R.id.content);
-                        // Process the response
-                        // TODO: API error message format
-                        if (response != null) {
-                            Snackbar.make(rootView, "Success", Snackbar.LENGTH_SHORT).show();
-                            Log.d("performSearchSuccess",response);
+            int responseCode = urlConnection.getResponseCode();
 
-                        } else {
-                            // Handle null or error response
-                            Snackbar.make(rootView, "Fail", Snackbar.LENGTH_SHORT).show();
-                            Log.d("performSearchFail",response);
-                            return;
-                        }
-                        // Hide loading indicator
-                        progressBar.setVisibility(View.GONE);
-                    }
-                });
-                // start a new activity to show the search result
-                Intent intent = new Intent(AtYourServiceActivity.this, SearchResultActivity.class);
-                // pass the response to the new activity
-                intent.putExtra("responseKey", response);
-                startActivity(intent);
-            } finally {
-                urlConnection.disconnect();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Handle a successful response
+                handleSuccessfulResponse(urlConnection);
+            } else {
+                // Handle HTTP error response
+                handleErrorResponse(urlConnection, responseCode);
             }
-        }catch (Exception e)
-        {
-            e.printStackTrace();
+        } catch (Exception e) {
+            // Handle exceptions
+            handleException(e);
+        }
+    }
+
+    private void handleSuccessfulResponse(HttpURLConnection connection) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            bufferedReader.close();
+            final String response = stringBuilder.toString();
+            Log.d("performSearchResponse", response);
+
             handler.post(new Runnable() {
                 @Override
                 public void run() {
                     View rootView = findViewById(android.R.id.content);
-                    // Handle exception
-                    Snackbar.make(rootView, "Fail", Snackbar.LENGTH_SHORT).show();
-                    Log.d("performSearchException",e.toString());
-                    // Hide loading indicator
+                    Snackbar.make(rootView, "Success", Snackbar.LENGTH_SHORT).show();
+                    Log.d("performSearchSuccess", response);
+                }
+            });
+
+            // Start a new activity to show the search result
+            Intent intent = new Intent(AtYourServiceActivity.this, SearchResultActivity.class);
+            intent.putExtra("responseKey", response);
+            startActivity(intent);
+        } catch (IOException e) {
+            handleException(e);
+        }
+    }
+
+    private void handleErrorResponse(HttpURLConnection connection, int responseCode) {
+        try {
+            String errorResponse = readErrorResponse(connection);
+            Log.d("performSearchError", errorResponse);
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    View rootView = findViewById(android.R.id.content);
+                    Snackbar.make(rootView, "HTTP Error: " + responseCode, Snackbar.LENGTH_SHORT).show();
                     progressBar.setVisibility(View.GONE);
                 }
             });
+        } catch (IOException e) {
+            handleException(e);
         }
     }
-}
 
+    private String readErrorResponse(HttpURLConnection connection) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        bufferedReader.close();
+        return stringBuilder.toString();
+    }
+
+    private void handleException(Exception e) {
+        e.printStackTrace();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                View rootView = findViewById(android.R.id.content);
+                Snackbar.make(rootView, "Fail", Snackbar.LENGTH_SHORT).show();
+                Log.d("performSearchException", e.toString());
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
