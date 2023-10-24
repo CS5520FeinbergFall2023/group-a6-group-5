6f7afe31807c6287c4f07613483233bc8ec1489f
kiranshatiya_5 <thirugnanasambanth.k@northeastern.edu>
2023-10-24 16:13:31 -0400

    Added the about us page with group member names and email

--- app/src/main/java/edu/northeastern/numad23fa23_group5/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa23_group5/MainActivity.java
@@ -21,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button aboutUsButton = findViewById(R.id.aboutUs);
+        aboutUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutUsActivity.class);
+
+                // Pass your name and email as extras to the AboutMeActivity
+                intent.putExtra("name1", "Ajay Inavolu");
+                intent.putExtra("email1", "inavolu.a@northeastern.edu");
+                intent.putExtra("name2", "Jiaming Xu");
+                intent.putExtra("email2", "xu.jiami@northeastern.edu");
+                intent.putExtra("name3", "Kiran Shatiya T R");
+                intent.putExtra("email3", "thirugnanasambanth.k@northeastern.edu");
+                intent.putExtra("name4", "Vishrutha Abbaiah Reddy");
+                intent.putExtra("email4", "abbaiahreddy.v@northeastern.edu");
+
+                // Start the AboutMeActivity
+                startActivity(intent);
+                }
+        });
     }
 }
