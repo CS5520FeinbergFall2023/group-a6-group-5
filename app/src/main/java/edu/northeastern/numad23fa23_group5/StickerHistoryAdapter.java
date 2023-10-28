f36f11fd6ef563ea095cfb066b01af132678456e
Jiaming Xu <xu.jiami@northeastern.edu>
2023-10-28 16:04:49 -0400

    change img width to match parent for better effect

--- app/src/main/java/edu/northeastern/numad23fa23_group5/StickerHistoryAdapter.java
+++ app/src/main/java/edu/northeastern/numad23fa23_group5/StickerHistoryAdapter.java
@@ -1,6 +1,7 @@
 package edu.northeastern.numad23fa23_group5;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
